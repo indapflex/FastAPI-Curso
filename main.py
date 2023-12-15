e509eb6cb0bde8dec277b6bd9c6e47a6900eeeb7
indapflex <indapflex@gmail.com>
2023-12-15 21:13:47 +0100

    Añado main

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from typing import Union
+
+from models.item_model import Item
+
+
+# Creación de una aplicación FastAPI
+app = FastAPI()
+
+#http://127.0.0.1:8000
+
+@app.get("/")
+def read_root():
+    return {"Hello" : "World!"}
+
+@app.get("/hola")
+def hola_mundo():
+    return {"Hola" : "Mundo"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None ):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/calculadora")
+def calcular(operando_1: float, operando_2: float):
+    return {"suma": operando_1 + operando_2}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item.id, "item_price": item.price}
